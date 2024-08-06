8b47b9226f3fc55e663a94a507b8fc3e5880d833
yopox <yopoxdev@gmail.com>
2024-08-06 10:37:16 +0200

    [components] add header

--- .eleventy.js
+++ .eleventy.js
@@ -1,30 +1,16 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addPassthroughCopy('src/img')
 
-
-  const {
-    DateTime
-  } = require("luxon");
-
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-      return DateTime.fromJSDate(dateObj, {
-        zone: 'utc'
-      }).toFormat('yy-MM-dd');
-    });
-
-    eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {
-      zone: 'utc'
-    }).toFormat("dd-MM-yy");
+  eleventyConfig.addFilter("imgFilter", name => {
+    return "img/" + name;
   });
 
   return {
-    dir: { input: 'src', output: '_site' }
+    dir: {input: 'src', output: '_site'}
   };
 };
