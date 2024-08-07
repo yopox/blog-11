45a388663ff7c284af8926bce7505ff98c344a50
yopox <yopoxdev@gmail.com>
2024-08-07 10:58:09 +0200

    [scripts] add color scroll script

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 import '../styles/main.css';
+
+const accent1 = [243, 217, 122];
+const accent2 = [255, 245, 200];
+
+function runOnScroll() {
+  let offset = window.scrollY / (document.body.offsetHeight - window.innerHeight);
+  offset = offset ** 2;
+  const color = [
+    accent1[0] + offset * (accent2[0] - accent1[0]),
+    accent1[1] + offset * (accent2[1] - accent1[1]),
+    accent1[2] + offset * (accent2[2] - accent1[2]),
+  ];
+  document
+    .querySelector(':root')
+    .style.setProperty(
+    '--accent',
+    `rgb(${color[0]},${color[1]},${color[2]})`
+  );
+}
+
+window.addEventListener('scroll', () => runOnScroll(), {passive: true});
