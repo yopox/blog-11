9d98d8de05449196e37418a0259c541b1b01edc3
Louis Vignier <mail@yopox.fr>
2024-08-14 10:52:27 +0200

    Create CNAME

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addPassthroughCopy('src/img')
+  eleventyConfig.addPassthroughCopy('CNAME')
 
   eleventyConfig.addFilter("imgFilter", name => {
     return "img/" + name
